91a18cb4073f5357e3d3c6020718b72f61da162d
jperaino <jprn72@gmail.com>
2017-06-08 15:54:50 -0400

    feat: add countdown clock

---
+++ Assets/Scripts/timeKeeper.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timeKeeper : MonoBehaviour {
+
+	Text timeText;
+	float timeRemaining;
+
+	// Use this for initialization
+	void Start () {
+		timeText = GetComponent<Text> ();
+		timeRemaining = 30.0f;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		timeRemaining -= Time.deltaTime;
+		timeText.text = timeRemaining.ToString("0.00");
+
+	}
+}
