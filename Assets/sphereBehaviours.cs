9d2bc5a783e251312a596f45505672d8401de997
jperaino <jprn72@gmail.com>
2017-06-08 15:08:10 -0400

    feat: spheres are reparented upon gaze
    This will facilitate scorekeeping in future updates.

--- Assets/sphereBehaviours.cs
+++ Assets/sphereBehaviours.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class sphereBehaviours : MonoBehaviour {
 
+
+
 	// Use this for initialization
 	void Start () {
-		
+
+
 	}
 	
 	// Update is called once per frame
@@ -15,9 +18,12 @@ public class sphereBehaviours : MonoBehaviour {
 	}
 
 	public void fallDown () {
+		GameObject spheresDone = GameObject.Find ("spheresDone");
+
 		//Debug.Log ("You called it");
 		this.GetComponent<Rigidbody> ().useGravity = true;
 		this.GetComponent<Rigidbody> ().constraints = RigidbodyConstraints.None;
+		this.gameObject.transform.parent = spheresDone.transform;
 
 	}
 }
