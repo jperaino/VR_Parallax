14dfac96262f62bf851c1309ffa8e9f2187fa96d
jperaino <jprn72@gmail.com>
2017-06-08 15:33:06 -0400

    feat: add scoreboard that updates when spheres are lost

---
+++ Assets/Scripts/scoreBoard.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class scoreBoard : MonoBehaviour {
+
+	Text scoreText;
+	int score;
+	public GameObject spheres;
+
+	// Use this for initialization
+	void Start () {
+
+		scoreText = GetComponent<Text> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		score = spheres.transform.childCount;
+		scoreText.text = score.ToString();
+
+	}
+}
