31e44afe1516324e98094a04b86d6c2509077112
jperaino <jprn72@gmail.com>
2017-06-08 21:48:40 -0400

    feat: make spheres fall when time runs out

--- Assets/Scripts/timeKeeper.cs
+++ Assets/Scripts/timeKeeper.cs
@@ -7,6 +7,7 @@ public class timeKeeper : MonoBehaviour {
 
 	Text timeText;
 	float timeRemaining;
+	int maxFall = 10;
 
 	// Use this for initialization
 	void Start () {
@@ -18,8 +19,58 @@ public class timeKeeper : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		timeRemaining -= Time.deltaTime;
-		timeText.text = timeRemaining.ToString("0.00");
+		if (timeRemaining > 0) {
+			timeRemaining -= Time.deltaTime;
+			timeText.text = timeRemaining.ToString ("0.00");
+
+		} else {
+
+			timeRemaining = 0;
+			timeText.text = "GAME OVER";
+			StartCoroutine ("allFallDown");
+		}
+
 
 	}
+
+
+	IEnumerator allFallDown () {
+
+		GameObject spheres = GameObject.Find ("spheres");
+		GameObject spheresDone = GameObject.Find ("spheresDone");
+		//Debug.Log ("here");
+
+		if (spheres.transform.childCount > maxFall) {
+			//Debug.Log ("here 2");
+
+			for (int i = 0; i < maxFall; i++) {
+
+				GameObject sphere = spheres.transform.GetChild (0).gameObject;
+
+				sphere.GetComponent<Rigidbody> ().useGravity = true;
+				sphere.GetComponent<Rigidbody> ().constraints = RigidbodyConstraints.None;
+				sphere.transform.parent = spheresDone.transform;
+			
+			}
+
+
+			//yield return new WaitForSeconds(1f);
+
+		} else if (spheres.transform.childCount > 0) {
+
+			GameObject sphere = spheres.transform.GetChild (0).gameObject;
+
+			sphere.GetComponent<Rigidbody> ().useGravity = true;
+			sphere.GetComponent<Rigidbody> ().constraints = RigidbodyConstraints.None;
+			sphere.transform.parent = spheresDone.transform;
+
+		} else {
+			yield break;
+		}
+
+
+	}
+
+
+
 }
--- Assets/sphereBehaviours.cs
+++ Assets/sphereBehaviours.cs
@@ -26,4 +26,7 @@ public class sphereBehaviours : MonoBehaviour {
 		this.gameObject.transform.parent = spheresDone.transform;
 
 	}
+		
+
+
 }
